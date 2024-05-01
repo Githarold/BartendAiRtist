f9471ae9057a7ccb321c52b364f9b355fe273b93
Kseorang1218 <switch1218@uos.ac.kr>
2024-05-01 21:10:24 +0900

    feat: 채팅(진행중)
    키보드 자판이 입력 창 안 가리게 하기

--- project/app/src/main/java/com/example/project/Chat.kt
+++ project/app/src/main/java/com/example/project/Chat.kt
@@ -1,43 +1,35 @@
 package com.example.project
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.project.databinding.ActivityChatBinding
 
 class Chat : AppCompatActivity() {
 
-    private  lateinit var receiverName: String
-
+    private lateinit var receiverName: String
     private lateinit var binding: ActivityChatBinding
     private lateinit var messageList: ArrayList<Message>
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChatBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
-        messageList = ArrayList()
-        val messageAdapter: MessageAdapter = MessageAdapter(this,messageList)
-
-        binding.chatRecyclerView.layoutManager=LinearLayoutManager(this)
-        binding.chatRecyclerView.adapter=messageAdapter
-
         setContentView(binding.root)
+
+        // 키보드가 활성화될 때 레이아웃 조정
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+
         receiverName = "쌈뽕한 바텐더"
         supportActionBar?.title = receiverName
-        messageList.clear()
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        messageList = ArrayList()
+        val messageAdapter: MessageAdapter = MessageAdapter(this, messageList)
+        binding.chatRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.chatRecyclerView.adapter = messageAdapter
 
-        binding.sendBtn.setOnClickListener{
+        binding.sendBtn.setOnClickListener {
             val messageText = binding.messageEdit.text.toString()
             if (messageText.isNotEmpty()) {
                 val message = Message(messageText)
