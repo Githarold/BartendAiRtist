287143017236626a9f94801e0da2dd61923c21a1
seongbin <czy105211@gmail.com>
2024-05-15 23:28:27 +0900

    bluetooth test2

--- server/processing.py
+++ server/processing.py
@@ -14,7 +14,7 @@ class protocol_1(threading.Thread):
     
     def run(self):
         print(self.cocktail_src)
-        self.socket.sendall(b"hello world")
+        self.socket.sendall("hello world")
         self.socket.close()
 
 class protocol_2(threading.Thread):
@@ -33,13 +33,16 @@ class protocol_2(threading.Thread):
             list_json(self.cocktail_src, PATH)            
             step_list, lin_list=protocol2serial(self.data)
             send_data_to_arduino(1,step_list, lin_list)
+            # time.sleep(5)
             print(step_list,lin_list)
             print(self.cocktail_src)
-            self.socket.sendall("hello world")
+            self.socket.sendall("ang")
             self.socket.close()
             self.sema.release()
         else:
             print("제작(스터링)을 위해서는 잠시 기다려주세요")
+            self.socket.sendall("wait")
+            self.socket.close()
         
 class protocol_3(threading.Thread):
     def __init__(self, socket, addr, data, cocktail_src, sema):
@@ -57,13 +60,16 @@ class protocol_3(threading.Thread):
             list_json(self.cocktail_src, PATH)
             step_list, lin_list=protocol2serial(self.data)
             send_data_to_arduino(0,step_list, lin_list)
+            # time.sleep(5)
             print(step_list,lin_list)
             print(self.cocktail_src)
-            self.socket.sendall(b"hello world")
+            self.socket.sendall("gimo ddi")
             self.socket.close()
             self.sema.release()
         else:
             print("제작(빌드)을 위해서는 잠시 기다려주세요")
+            self.socket.sendall("wait")
+            self.socket.close()
 
 class protocol_4(threading.Thread):
     def __init__(self, socket, addr, data, cocktail_src, sema):
@@ -80,8 +86,10 @@ class protocol_4(threading.Thread):
             self.cocktail_src[1]+=self.data.content[1]
             list_json(self.cocktail_src, PATH)
             print(self.cocktail_src)
-            self.socket.sendall(b"hello world")
+            self.socket.sendall("cnrk")
             self.socket.close()
             self.sema.release()
         else:
             print("추가를 위해서는 잠시 기다려주세요")
+            self.socket.sendall("wait")
+            self.socket.close()
