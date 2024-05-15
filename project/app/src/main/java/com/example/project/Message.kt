7f818a20230e438a9470c937206cb7118c3c6a26
Kseorang1218 <switch1218@uos.ac.kr>
2024-05-15 23:50:54 +0900

    feat: 채팅-챗gpt 연동(진행중)

--- project/app/src/main/java/com/example/project/Message.kt
+++ project/app/src/main/java/com/example/project/Message.kt
@@ -1,8 +1,18 @@
 package com.example.project
 
-data class Message(
-    var message: String?
+class Message {
+    var message: String? = null
+    var sentBy: String? = null
 
-){
-    constructor():this("")
+    constructor(message: String?, sentBy: String?) {
+        this.message = message
+        this.sentBy = sentBy
+    }
+
+    constructor()
+
+    companion object {
+        var SENT_BY_ME = "me"
+        var SENT_BY_BOT = "bot"
+    }
 }
