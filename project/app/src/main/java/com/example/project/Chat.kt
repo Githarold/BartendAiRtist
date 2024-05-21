7eb3d326b3a520e9ae43187c911dcb5307ce0f46
Kseorang1218 <switch1218@uos.ac.kr>
2024-05-21 15:13:59 +0900

    refactor: 채팅-챗gpt 연동(진행중4)
    추천 기능(gpt 4-turbo), openai API 이용해 구현
    adjust_pumps 구현 이전 추천기능만 작

--- project/app/src/main/java/com/example/project/Chat.kt
+++ project/app/src/main/java/com/example/project/Chat.kt
@@ -23,6 +23,22 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONArray
 import com.google.gson.Gson
 import java.util.concurrent.TimeUnit
+import com.aallam.openai.*
+import com.aallam.openai.api.BetaOpenAI
+import com.aallam.openai.client.*
+import com.aallam.openai.client.OpenAI
+import com.aallam.openai.api.assistant.*
+import com.aallam.openai.api.core.RequestOptions
+import com.aallam.openai.api.core.Role
+import com.aallam.openai.api.core.Status
+import com.aallam.openai.api.http.Timeout
+import com.aallam.openai.api.message.MessageContent
+import com.aallam.openai.api.message.MessageRequest
+import com.aallam.openai.api.model.ModelId
+import com.aallam.openai.api.run.RunRequest
+import com.aallam.openai.api.thread.ThreadId
+import kotlinx.coroutines.*
+import kotlin.time.Duration.Companion.seconds
 
 class Chat : AppCompatActivity() {
     var recycler_view: RecyclerView? = null
@@ -66,7 +82,9 @@ class Chat : AppCompatActivity() {
     fun sendMessage(){
         val question = message_edit!!.getText().toString().trim { it <= ' ' }
         addToChat(question, Message.SENT_BY_ME)
-        callAPI(question)
+        CoroutineScope(Dispatchers.Main).launch {
+            callAPI(question)
+        }
         message_edit!!.text.clear()
         tv_welcome!!.setVisibility(View.GONE)
     }
@@ -84,85 +102,135 @@ class Chat : AppCompatActivity() {
         addToChat(response, Message.SENT_BY_BOT)
     }
 
-    fun callAPI(question: String?) {
-        //okhttp
-        val prompt = generate_cocktail_recipe(question.toString())
-
+    @OptIn(BetaOpenAI::class)
+    suspend fun callAPI(question: String?){
         messageList!!.add(Message("...", Message.SENT_BY_BOT))
-        val obj = JSONObject()
-        try {
-            obj.put("model", "gpt-3.5-turbo")
-            obj.put("messages", JSONArray().put(JSONObject().put("role", "system").put("content", prompt))) // 객체 배열로 변경
-            obj.put("max_tokens", 500)
-        } catch (e: JSONException) {
-            e.printStackTrace()
+
+        val openai = OpenAI( token = MY_SECRET_KEY)
+        // Few-shot Learning을 위한 예시 입력 선언
+        val exampleInputDict = mapOf(
+            "Vodka" to 700, "Rum" to 700, "Gin" to 700, "Diluted Lemon Juice" to 1000,
+            "Triple Sec" to 500, "Cranberry Juice" to 1000, "Grapefruit Juice" to 1000, "Orange Juice" to 800
+        )
+
+        val realInputDict = mapOf(
+            "Vodka" to 1000, "Rum" to 700, "Gin" to 800, "Diluted Lemon Juice" to 800,
+            "Triple Sec" to 500, "Cranberry Juice" to 900, "Grapefruit Juice" to 1000, "Orange Juice" to 800
+        )
+
+
+        val exampleUserMood1 = "오늘따라 뭔가 시원하고 달콤한 칵테일이 마시고 싶어. 피로도 풀리고 기분도 좋아지는 그런 종류로."
+        val exampleGptResponse1 = "그런 기분에 딱 맞는 칵테일로 '모히토'를 추천드릴게요. 신선한 민트와 라임이 들어가 상큼하고 시원한 맛이 특징이랍니다. 럼 주를 기반으로 해서 달콤한 맛도 느끼실 수 있고요.@[{'Rum': 1, 'Diluted Lemon Juice': 2, 'Orange Juice': 3}, {'Rum': 2, 'Diluted Lemon Juice': 3, 'Orange Juice': 4}]"
+
+        val exampleUserMood2 = "오늘 정말 무더운 날이네요. 뭔가 시원하고 상쾌한 음료가 마시고 싶어요."
+        val exampleGptResponse2 = "열대의 상쾌함을 원하신다면 '트로피칼 펀치'를 추천드리고 싶어요. 이 칵테일은 자몽 주스와 오렌지 주스를 기반으로 하고 있어서 상큼하고, 럼과 트리플 섹이 더해져 풍미가 풍부합니다. 정말 열대 지방의 분위기를 느낄 수 있죠.@[{'Rum': 1, 'Orange Juice': 2, 'Grapefruit Juice': 3, 'Triple Sec': 4, 'Cranberry Juice': 5}, {'Rum': 2, 'Orange Juice': 4, 'Grapefruit Juice': 4, 'Triple Sec': 1, 'Cranberry Juice': 2}]"
+
+        val exampleUserMood3 = "주말 파티를 위해 강렬한 칵테일이 필요해요."
+        val exampleGptResponse3 = "주말 파티에는 '마가리타'를 추천드릴게요. 테킬라와 라임 주스, 트리플 섹이 조화를 이루어 강렬하면서도 상쾌한 맛을 느낄 수 있습니다.@[{'Tequila': 1, 'Triple Sec': 1, 'Lime Juice': 1}, {'Tequila': 2, 'Triple Sec': 2, 'Lime Juice': 2}]"
+
+        val exampleUserMood4 = "친구와 함께 마실 수 있는 달콤한 칵테일을 추천해줘."
+        val exampleGptResponse4 = "친구와 함께 마시기 좋은 칵테일로 '피치 벨리니'를 추천드릴게요. 피치 퓌레와 샴페인이 어우러져 달콤하고 청량한 맛을 느낄 수 있습니다.@[{'Peach Puree': 1, 'Champagne': 3}, {'Peach Puree': 1, 'Champagne': 4}]"
+
+        val exampleUserMood5 = "저녁 식사 후에 마실 수 있는 부드러운 칵테일을 원해요."
+        val exampleGptResponse5 = "저녁 식사 후에는 '화이트 러시안'을 추천드릴게요. 보드카, 커피 리큐어, 크림이 어우러져 부드럽고 진한 맛을 느낄 수 있습니다.@[{'Vodka': 2, 'Coffee Liqueur': 1, 'Cream': 1}, {'Vodka': 3, 'Coffee Liqueur': 2, 'Cream': 2}]"
+
+        // AI 바텐더 Assistant 생성
+        val batender = openai.assistant(
+            request = AssistantRequest(
+                name = "AI Bartender",
+                model = ModelId("gpt-4-turbo"),
+                instructions = """
+                    You are an AI bartender. First, receive the inventory as a dictionary named 'example_dict',
+                    then consider the user's mood and preferences to recommend a cocktail.
+                    Ensure that the total volume of ingredients does not exceed 250ml. Use a specific delimiter (@) to separate the cocktail recommendation from the recipe,
+                    which should be provided in a structured list format, including two dictionaries:
+                    one for the "Order" of ingredients and another for the "Integer" number of 30ml pumps required for each ingredient.
+                """.trimIndent()
+            )
+        )
+
+        // 대화를 관리할 Thread 생성
+        val thread = openai.thread()
+        println(thread.id)
+
+        val real_user_mood = question
+        
+        openai.message(
+            threadId = thread.id,
+            request = MessageRequest(
+                role = Role.User,
+                content = real_user_mood!!
+            )
+        )
+
+        val messages = openai.messages(thread.id)
+        println("List of messages in the thread:")
+        for (message in messages) {
+            val textContent = message.content.first() as? MessageContent.Text ?: error("Expected MessageContent.Text")
+            println(textContent.text.value)
         }
 
-        val body = obj.toString().toRequestBody(JSON)
+        val run = openai.createRun(
+                threadId = thread.id,
+                request = RunRequest(
+                    assistantId = batender.id,
+                    instructions ="""
+                          Example 1:
+                            Input: Inventory - $exampleInputDict, Mood/Preference - '$exampleUserMood1'
+                            Output: $exampleGptResponse1
+                            
+                            Example 2:
+                            Input: Inventory - $exampleInputDict, Mood/Preference - '$exampleUserMood2'
+                            Output: $exampleGptResponse2
+                        
+                            Example 3:
+                            Input: Inventory - $exampleInputDict, Mood/Preference - '$exampleUserMood3'
+                            Output: $exampleGptResponse3
+                        
+                            Example 4:
+                            Input: Inventory - $exampleInputDict, Mood/Preference - '$exampleUserMood4'
+                            Output: $exampleGptResponse4
+                        
+                            Example 5:
+                            Input: Inventory - $exampleInputDict, Mood/Preference - '$exampleUserMood5'
+                            Output: $exampleGptResponse5
+                            
+                            You have to respond in Korean:
+                            Input: Inventory - $realInputDict, Mood/Preference - '$real_user_mood'
+                            Output: 
+                                            """.trimIndent())
+            )
 
-//        println(prompt)
-        val request: Request = Builder()
-            .url("https://api.openai.com/v1/chat/completions")
-            .header("Authorization", "Bearer " + MY_SECRET_KEY)
-            .post(body)
-            .build()
+        do {
+            delay(1500)
+            val retrievedRun = openai.getRun(threadId = thread.id, runId = run.id)
+        } while (retrievedRun.status != Status.Completed)
 
-        client.newCall(request).enqueue(object : okhttp3.Callback {
-            override fun onFailure(call: okhttp3.Call, e: IOException) {
-                addResponse("Failed to load response due to " + e.message)
+        val assistantMessages = openai.messages(thread.id)
+        val message = assistantMessages[0]
+        val textContent = message.content.first() as? MessageContent.Text ?: error("Expected MessageContent.Text")
+        val messageText = textContent.text.value
+        println(messageText)
+
+        if (messageText != null && messageText.isNotEmpty()){
+            val parts = messageText.split("@")
+            if (parts.size > 1) {
+                val recommendReason = parts[0]
+                val recipeString = parts[1]
+                addResponse(recommendReason)
+            } else {
+                addResponse(messageText)
             }
+        }else{
+            addResponse("Err.. Try again")
+        }
 
-            override fun onResponse(call: okhttp3.Call, response: okhttp3.Response) {
-                if (response.isSuccessful) {
-                    println(response)
-                    var jsonObject: JSONObject? = null
-                    try {
-                        val responseBody = response.body
-                        val responseBodyString = responseBody?.string()
-                        jsonObject = JSONObject(responseBodyString)
-                        var jsonArray = jsonObject?.getJSONArray("choices")
-                        val result = jsonArray?.getJSONObject(0)?.getString("message")
-                        val content = JSONObject(result).getString("content")
-                        addResponse(content?.trim { it <= ' ' })
-                    } catch (e: JSONException) {
-                        e.printStackTrace()
-                    }
-                } else {
-                    addResponse("Failed to load response due to " + response.body?.string())
-                }
-            }
 
-        })
     }
+
 
     companion object {
         val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
         private const val MY_SECRET_KEY = ""
     }
-}
-
-fun generate_cocktail_recipe(userMood: String?): String{
-    val example1 = mapOf(
-        "input" to "기분이 우울할 때",
-        "output" to mapOf(
-            "name" to "블루 라군",
-            "ingredients" to listOf("보드카", "블루 큐라소", "레몬 주스", "소다"),
-            "reason" to "상쾌한 맛이 기분을 전환시켜 줍니다."
-        )
-    )
-
-    val example2 = mapOf(
-        "input" to "기분이 행복할 때",
-        "output" to mapOf(
-            "name" to "마가리타",
-            "ingredients" to listOf("데킬라", "트리플 섹", "라임 주스"),
-            "reason" to "새콤달콤한 맛이 기쁜 기분을 더욱 돋보이게 합니다."
-        )
-    )
-
-    var prompt = "Input: ${example1["input"]}\nOutput: ${Gson().toJson(example1["output"])}\n\n"
-    prompt += "Input: ${example2["input"]}\nOutput: ${Gson().toJson(example2["output"])}\n\n"
-    prompt += "Input: $userMood\nOutput:"
-    return prompt
-
 }
